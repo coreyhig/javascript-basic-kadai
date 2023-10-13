a24c26637d1253f88d450d397bafb58991b85ebe
coreyhig <143170162+coreyhig@users.noreply.github.com>
2023-10-13 21:02:49 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,5 +2,5 @@ const change = document.getElementById('btn');
 
 
   change.addEventListener('click', ()=>{
-    document.getElementById('text').innerHTML = '<id="text">ボタンをクリックしました</>';
+    document.getElementById('text').innerHTML = 'ボタンをクリックしました';
   });
